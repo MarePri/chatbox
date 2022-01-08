da3e256bccc0b6162c87fb067c108abffd63ef45
MarePri <marko.cena@gmail.com>
2022-01-08 19:39:33 +0100

    bump

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import useInterval from '@use-it/interval'
+import { animate, motion } from 'framer-motion';
 import './App.css';
 
 const messages = [
@@ -12,19 +14,58 @@ const messages = [
 ];
 
 export default function App() {
+  const [messageToShow, setMessageToShow] = useState(0);
+
+  useInterval(() => {
+    setMessageToShow(messageToShow => messageToShow + 1);
+  }, 2000)
+
   return (
     <div className="app">
       <div className="walkthrough">
         {messages.map((message, index) => {
+          const even = index % 2 ===0;
+
+          if (messageToShow + 1 === index) {
+            return <Typing key={index} even={even} />;
+          }
+
+
+          if (index > messageToShow) return <div key={index} />
+
+
+
+
+
           return (
-            <div key={index} className="message">
-              <div className='avatar'>🌸 </div>
-              <div className='text'>{message.text}</div>
-              <div className='avatar'>😊 </div>
-            </div>
+            <Message key={index} message={message}/>
           );
         })}
       </div>
     </div>
   );
+}
+
+
+function Typing({even}) {
+  return (
+    <div className={`typing ${even ? 'is-left' : 'is-right' }`}>
+      <div className="dots">
+        <div />
+        <div />
+        <div />
+      </div>
+    </div>
+  )
+}
+
+function Message({message}) {
+
+  return(
+    <motion.div className="message" initial={{scale: 0.2}} animate={{scale:1}}>
+              <div className='avatar'>🌸 </div>
+              <div className='text'>{message.text}</div>
+              <div className='avatar'>😊 </div>
+            </motion.div>
+  )
 }
